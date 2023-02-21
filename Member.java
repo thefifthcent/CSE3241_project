de032a0ad5589fc3e9a2b8be9e4665dae89853bb
Nikhil Gupta <ncgupta216@gmail.com>
2023-02-21 12:03:40 -0500

    Framework of Main and Member

---
+++ Member.java
@@ -0,0 +1,27 @@
+public class Member {
+    
+    // Attributes
+    String fName;
+    String lName;
+    String address;
+    String phoneNumber;
+    String email;
+    String startDate;
+    String endDate;
+    double warehouseDistance;
+    int userID;
+    
+    // Constructor
+    public Member() {
+        fName = "";
+        lName = "";
+        address = "";
+        phoneNumber = "";
+        email = "";
+        startDate = "";
+        endDate = "";
+        warehouseDistance = 0.0;
+        userID = 0;
+    }
+    
+}
--- main.java
+++ main.java
@@ -3,3 +3,10 @@
  * 
  * @Authors: Jessie, Yuhao. Alaina, Nikhil
  */
+
+ public class Main {
+
+    public static void main(String[] args) {
+        
+    }
+ }
