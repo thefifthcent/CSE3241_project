0b6acb22ce93c008ac190f16e4e34a6f4a7eee45
jessieshen12 <cicijessie12@gmail.com>
2023-02-21 15:20:44 -0500

    Prompts user for a String

---
+++ mainFile.java
@@ -0,0 +1,33 @@
+/**
+ * Database Project
+ * 
+ * @Authors: Jessie, Yuhao. Alaina, Nikhil
+ */
+
+ import java.util.Scanner;
+
+ public class mainFile {
+   static int currentUserID = 0;
+   public static void main(String[] args) {
+      Scanner in = new Scanner(System.in); //Scanner object
+
+      System.out.println("Welcome!\n");
+
+      System.out.println("Would you like to add, edit/delete or search a record?");
+      String userInput = in.nextLine();
+
+      while(true) {
+        if(userInput.toLowerCase().equals("add")) {
+
+        } else if(userInput.toLowerCase().equals("edit/delete")) {
+
+        } else if(userInput.toLowerCase().equals("search")) {
+
+        } else {
+          System.out.println("Not a valid input. Type \"add\", \"edit/delete\" or \"search\"");
+          userInput = in.nextLine();
+        }
+      }
+
+   }
+ }
